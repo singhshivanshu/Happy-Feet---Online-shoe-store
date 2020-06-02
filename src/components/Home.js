c0497586b59f2f336283f40664a17c72a180bcfc
singhshivanshu <shivanshusingh12@gmail.com>
2020-06-02 17:10:41 +0530

    feat: added json data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from "react"
+import Header from "./Header"
 
 function Home() {
     return (
-        <div>
-
-        </div>
+       <React.Fragment>
+           <Header/>
+       </React.Fragment>
     )
 }
 
